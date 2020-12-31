ba7f3ead0e149ce8fb0aa0535a1dc85e989f08f5
KrystalY <yoosj0703@gmail.com>
2020-12-31 22:19:25 +0900

    test: unstated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app/App';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { createContainer } from "unstated-next";
+//import App from './components/app/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function useCounter(initialState = 0) {
+  let [count, setCount] = useState(initialState);
+  let decrement = () => setCount(count - 1);
+  let increment = () => setCount(count + 1);
+  return { count, decrement, increment };
+}
+
+let Counter = createContainer(useCounter);
+
+function CounterDisplay() {
+  let counter = Counter.useContainer();
+  return (
+    <div>
+      <button onClick={counter.decrement}>+</button>
+      <span>{counter.count}</span>
+      <button onClick={counter.decrement}>-</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Counter.Provider>
+      <CounterDisplay />
+      <Counter.Provider initialState={2}>
+        <div>
+          <div>
+            <CounterDisplay />
+          </div>
+        </div>
+      </Counter.Provider>
+    </Counter.Provider>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
